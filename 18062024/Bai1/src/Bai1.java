da131a60565e843822d344d4a37ea4e4950b2e6a
Rains97 <33325525+hvinhtuong@users.noreply.github.com>
2024-06-19 16:35:26 +0700

    Add files via upload

--- 18062024/Bai1/src/Bai1.java
+++ 18062024/Bai1/src/Bai1.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
+
 public class Bai1 {
+    static int s;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Type your speed: ");
-        int s = sc.nextInt();
+        do {
+            System.out.print("Type your speed (must be negative): ");
+            s = sc.nextInt();
+        } while (s < 0);
         Car car = new Car(s);
         Bicycle bicycle = new Bicycle(s);
-        if (s>0) {
             if (s >= 100) {
                 car.speedUp();
             } else {
                 bicycle.speedUp();
             }
-        } else {
-            System.out.println("Error value.");
         }
     }
-}
