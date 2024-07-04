9964fbace3d679204432b607da8456457540bfc1
Rains97 <33325525+hvinhtuong@users.noreply.github.com>
2024-07-04 19:10:08 +0700

    Add files via upload

--- 04072024/Baitap/src/Baitapchieu/Bai2/Bai2.java
+++ 04072024/Baitap/src/Baitapchieu/Bai2/Bai2.java
@@ -8,49 +8,46 @@ import java.util.List;
 
 public class Bai2 {
 
-
-    /*
-Sort method 3 bubbleSort
- */
      /*
       * bubbleSort - Sort list
       */
-    static void bubbleSort(List<Integer> list) {
+    static List<Integer> bubbleSort(List<Integer> list) {
+        List<Integer> sortedList = new ArrayList<>(list);
         int i, j;
-        int temp;
         boolean swap;
-        for (i = 0; i < list.size() - 1; i++) {
+        for (i = 0; i < sortedList.size() - 1; i++) {
             swap = false;
-            for (j = 0; j < list.size() - i - 1; j++) {
-                if (list.get(i) > list.get(j+1)) {
-                    // swap
-                    temp = list.get(j);
-                    list.set(j, list.get(j + 1));
-                    list.set(j + 1, temp);
+            for (j = 0; j < sortedList.size() - i - 1; j++) {
+                if (sortedList.get(j) > sortedList.get(j + 1)) {
+                    // Swap
+                    int temp = sortedList.get(j);
+                    sortedList.set(j, sortedList.get(j + 1));
+                    sortedList.set(j + 1, temp);
                     swap = true;
                 }
             }
-            if (swap == false)
+            if (swap == false) {
                 break;
+            }
         }
+        return sortedList;
     }
 
     public static void toDo(List<Integer> list, IBai2 s) {
-        getSecond(list);
+        s.findSeconmax(list);
     }
 
      /*
       * getSecond - Find and print 2nd maximum of array and minimum
       */
     public static void getSecond(List<Integer> list) {
-        bubbleSort(list);
-        System.out.println("So lon thu hai trong day la: " + list.getFirst());
-        System.out.println("So nho nhat cua day la: " + list.get(1));
+        System.out.println("So lon thu hai trong day la: " + bubbleSort(list).get(list.size()-2));
+        System.out.println("So nho nhat cua day la: " + bubbleSort(list).getFirst());
     }
 
     public static void main(String[] args) {
-    List<Integer> list = Arrays.asList(10, 8, 1, 3, 5, 7, 4, 2, 6);
-    System.out.println("Your array: " + (list));
+    List<Integer> list = Arrays.asList(10, 8, 1, 3, 5, 7, 4, 2, 6, 0);
+    System.out.println("Your array: " + list);
     toDo(list, Bai2::getSecond);
     }
 }
