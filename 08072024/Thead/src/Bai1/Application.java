3e9ee030e4811f0d55d947e3f30be96b866d58a4
Rains97 <33325525+hvinhtuong@users.noreply.github.com>
2024-07-08 16:02:43 +0700

    Add files via upload

--- 08072024/Thead/src/Bai1/Application.java
+++ 08072024/Thead/src/Bai1/Application.java
@@ -7,11 +7,11 @@ public class Application {
     public static List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
 
     public static void main(String[] args) throws InterruptedException {
-        Thread a = new Thread(new firstThread());
-        Thread b = new Thread(new twoThread());
+        Thread odd = new Thread(new firstThread());
+        Thread even = new Thread(new twoThread());
 
-        a.start();
-        b.start();
+        odd.start();
+        even.start();
 
     }
 }
