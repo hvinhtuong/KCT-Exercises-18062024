f680fcd2db65945194bf5f59063b13ae4c96a8f0
Rains97 <33325525+hvinhtuong@users.noreply.github.com>
2024-07-29 06:31:43 +0700

    Create Schema.sql

---
+++ 22072024/OfficeDemo/src/main/java/vn/edu/likelion/OfficeDemo/WarehouseManager/Schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Account (
+    id INTEGER PRIMARY KEY,
+    user_name NVARCHAR2(100) NOT NULL,
+    password NVARCHAR2(100) NOT NULL,
+    role INTEGER,
+    status INTEGER
+);
+
+CREATE TABLE Warehouses (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER unique,
+    name NVARCHAR2(100) NOT NULL,
+    category INTEGER,
+    FOREIGN KEY (user_id) REFERENCES Account(id)
+);
+
+CREATE TABLE Item (
+    id INTEGER PRIMARY KEY,
+    warehouse_id INTEGER,
+    name NVARCHAR2(100) NOT NULL,
+    detail NVARCHAR2(100) NOT NULL,
+    price NUMBER(10,2),
+    amount INTEGER,
+    FOREIGN KEY (warehouse_id) REFERENCES Warehouses(id)
+);
