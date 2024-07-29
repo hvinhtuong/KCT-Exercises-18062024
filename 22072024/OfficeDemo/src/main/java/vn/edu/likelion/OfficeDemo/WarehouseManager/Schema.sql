213984e8654a4ebed1957717c4970dbd4d2973c5
Rains97 <33325525+hvinhtuong@users.noreply.github.com>
2024-07-29 07:34:38 +0700

    Update Schema.sql

--- 22072024/OfficeDemo/src/main/java/vn/edu/likelion/OfficeDemo/WarehouseManager/Schema.sql
+++ 22072024/OfficeDemo/src/main/java/vn/edu/likelion/OfficeDemo/WarehouseManager/Schema.sql
@@ -14,12 +14,30 @@ CREATE TABLE Warehouses (
     FOREIGN KEY (user_id) REFERENCES Account(id)
 );
 
-CREATE TABLE Item (
+
+CREATE TABLE Account (
     id INTEGER PRIMARY KEY,
-    warehouse_id INTEGER,
+    user_name NVARCHAR2(100) NOT NULL,
+    password NVARCHAR2(100) NOT NULL,
+    role INTEGER,
+    status INTEGER
+);
+
+CREATE TABLE Warehouses (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER unique,
+    name NVARCHAR2(100) NOT NULL,
+    category INTEGER,
+    FOREIGN KEY (user_id) REFERENCES Account(id)
+);
+
+CREATE TABLE Item (
+    id INTEGER NOT NULL,
+    warehouse_id INTEGER NOT NULL,
     name NVARCHAR2(100) NOT NULL,
     detail NVARCHAR2(100) NOT NULL,
     price NUMBER(10,2),
     amount INTEGER,
+    PRIMARY KEY (id, warehouse_id),
     FOREIGN KEY (warehouse_id) REFERENCES Warehouses(id)
 );
